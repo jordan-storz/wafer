29e9234c417dc410e4743f5ecad7a100e986d628
Michael Roque <mikeroque1@gmail.com>
2016-12-02 14:49:00 -0700

    Request works

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/wafers', function(req, res) {
-  console.log('POST REQ RECEIVED');
-})
+
+    app.post('/wafers', function(req, res) {
+      res.send('test')
+      console.log(req.body);
+      console.log('POST REQ RECEIVED');
+    })
 
 
 app.listen(3000, function() {
-  console.log('SERVER RUNNING');
+    console.log('SERVER RUNNING');
 })
